c9f29cbe6f5350a46dba305849c497c497446558
Koiiwa <koiiwa666@gmail.com>
2026-05-24 17:21:20 +0700

    added some media and optimized viewing

--- GarbageDetector.py
+++ GarbageDetector.py
@@ -10,8 +10,8 @@ model = YOLO("bestv2.pt")
 
 class_names = ['Aerosol', 'Aluminium blister pack', 'Aluminium foil', 'Battery', 'Broken glass', 'Carded blister pack', 'Cigarette', 'Clear plastic bottle', 'Corrugated carton', 'Crisp packet', 'Disposable food container', 'Disposable plastic cup', 'Drink can', 'Drink carton', 'Egg carton', 'Foam cup', 'Foam food container', 'Food Can', 'Food waste', 'Garbage bag', 'Glass bottle', 'Glass cup', 'Glass jar', 'Magazine paper', 'Meal carton', 'Metal bottle cap', 'Metal lid', 'Normal paper', 'Other carton', 'Other plastic', 'Other plastic bottle', 'Other plastic container', 'Other plastic cup', 'Other plastic wrapper', 'Paper bag', 'Paper cup', 'Paper straw', 'Pizza box', 'Plastic bottle cap', 'Plastic film', 'Plastic glooves', 'Plastic lid', 'Plastic straw', 'Plastic utensils', 'Polypropylene bag', 'Pop tab', 'Rope & strings', 'Scrap metal', 'Shoe', 'Single-use carrier bag', 'Six pack rings', 'Spread tub', 'Squeezable tube', 'Styrofoam piece', 'Tissues', 'Toilet tube', 'Tupperware', 'Unlabeled litter', 'Wrapping paper']
 
-target_filename = "garbage_1.jpg"  
-project_root = "/Users/meimei/Documents/CTU/Project/GarbageDetector/Media"
+target_filename = "garbage_7.jpeg"  
+project_root = "/Users/meimei/Documents/CTU/CT190E/GarbageDetector/Media"
 image_path = None
 
 for root, dirs, files in os.walk(project_root):
--- GarbageDetectorLive.py
+++ GarbageDetectorLive.py
@@ -9,13 +9,13 @@ model = YOLO("bestv2.pt")
 
 class_names = ['Aerosol', 'Aluminium blister pack', 'Aluminium foil', 'Battery', 'Broken glass', 'Carded blister pack', 'Cigarette', 'Clear plastic bottle', 'Corrugated carton', 'Crisp packet', 'Disposable food container', 'Disposable plastic cup', 'Drink can', 'Drink carton', 'Egg carton', 'Foam cup', 'Foam food container', 'Food Can', 'Food waste', 'Garbage bag', 'Glass bottle', 'Glass cup', 'Glass jar', 'Magazine paper', 'Meal carton', 'Metal bottle cap', 'Metal lid', 'Normal paper', 'Other carton', 'Other plastic', 'Other plastic bottle', 'Other plastic container', 'Other plastic cup', 'Other plastic wrapper', 'Paper bag', 'Paper cup', 'Paper straw', 'Pizza box', 'Plastic bottle cap', 'Plastic film', 'Plastic glooves', 'Plastic lid', 'Plastic straw', 'Plastic utensils', 'Polypropylene bag', 'Pop tab', 'Rope & strings', 'Scrap metal', 'Shoe', 'Single-use carrier bag', 'Six pack rings', 'Spread tub', 'Squeezable tube', 'Styrofoam piece', 'Tissues', 'Toilet tube', 'Tupperware', 'Unlabeled litter', 'Wrapping paper']
 
-#target_filename = "London2.mp4"
+target_filename = "London2.mp4"
 #target_filename = "trashAroundLondon.mp4"
 #target_filename = "beach.mp4"
 #target_filename = "trashOnWater.mp4"
-target_filename = "garbage.mp4"
+#target_filename = "garbage.mp4"
 
-project_root = "/Users/meimei/Documents/CTU/Project/GarbageDetector"
+project_root = "/Users/meimei/Documents/CTU/CT190E/GarbageDetector/Media"
 video_path = None
 
 for root, dirs, files in os.walk(project_root):
